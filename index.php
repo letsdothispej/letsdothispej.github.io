8dec239b613944c899e14b8d28236c71691b52aa
letsdothispej <azanespaul@gmail.com>
2017-04-16 02:28:06 +0800

    lil changes

---
+++ index.php
@@ -0,0 +1,33 @@
+
+
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<title>Awesome Website</title>
+<link rel="stylesheet" type="text/css" href="">
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">Features</a></li>
+				<li><a href="">Pricing</a></li>
+				<li><a href="">Support</a></li>
+				<li><a href="">Blog</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<div>
+		<h1>Hello World</h1>
+		<p>This is a try</p>
+		<?php
+	include 'php/try.php';
+		?>
+		<form class="forbtn" action="php/main.php">
+		<button class="btn" type="submit">Press to send mail</button>
+		</form>
+	</div>
+		
+</body>
+</html>
