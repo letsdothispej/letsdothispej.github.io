9f3615b9ea505d302b12e02ece203eea5b88ca4f
letsdothispej <azanespaul@gmail.com>
2017-04-15 14:35:54 +0800

    Fully functioned in offline

--- php/main.php
+++ php/main.php
@@ -1,10 +1,10 @@
 <?php
+date_default_timezone_set('Asia/Manila');
+
 $to = "pheej.azanes@gmail.com";
 $subject = "My subject";
 $txt = "Hello world!";
 $headers = "From: azanespaul@gmail.com";
 
 mail($to,$subject,$txt,$headers);
-
-echo "Working";
 ?>
